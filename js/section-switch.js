7b99e2f72c087f6581f6e72504e33b6e5df4c991
NissanJK <jawadul.karim78@gmail.com>
2024-09-25 00:24:12 +0600

    switching between donation page and history page done

---
+++ js/section-switch.js
@@ -0,0 +1,8 @@
+document.getElementById('donation-btn')
+    .addEventListener('click', function () {
+        showSectionById('donation-section');
+    });
+document.getElementById('history-btn')
+    .addEventListener('click', function () {
+        showSectionById('history-section');
+    });
--- js/utilities.js
+++ js/utilities.js
@@ -9,3 +9,9 @@ function getTextFieldValueById(id) {
     const textNumber = parseFloat(textValue);
     return textNumber;
 }
+
+function showSectionById(id) {
+    document.getElementById('donation-section').classList.add('hidden');
+    document.getElementById('history-section').classList.add('hidden');
+    document.getElementById(id).classList.remove('hidden');
+}
